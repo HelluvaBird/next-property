6895cc1de42bce8b30e7249e1ab2df11a96844d5
HelluvaBird <royceman24@hotmail.com>
2024-05-28 20:32:49 -0400

    fetch bookmarks for bookmark profile page

--- app/profile/@profile/bookmarks/page.tsx
+++ app/profile/@profile/bookmarks/page.tsx
@@ -1,5 +1,8 @@
-export default function Bookmarks() {
-  const bookmarks: Array<{ id: string; name: string; street: string }> = [];
+import { getProfileBookmarks } from '@/app/action';
+import Link from 'next/link';
+
+export default async function Bookmarks() {
+  const bookmarks = await getProfileBookmarks();
   return (
     <div className="mx-auto max-w-2xl lg:mx-0 lg:max-w-none">
       <h2 className="leading-7 font-semibold text-base text-gray-900">
@@ -10,14 +13,19 @@ export default function Bookmarks() {
       </p>
 
       <dl className="mt-8 border-t border-gray-200 text-sm leading-6 space-y-6 divide-y divide-gray-100">
-        {bookmarks.length > 0 ? (
+        {bookmarks ? (
           bookmarks?.map((bookmark) => (
-            <div key={bookmark.id} className="pt-6 sm:flex">
-              <dt className="font-medium sm:flex-none sm:w-64 sm:pe-6 text-gray-900">
-                {bookmark.name}
-              </dt>
+            <div key={bookmark.propertyId} className="pt-6 sm:flex">
+              <Link
+                href={`/property/${bookmark.propertyId}`}
+                className="text-rose-400 hover:text-rose-500"
+              >
+                <dt className="font-medium sm:flex-none sm:w-64 sm:pe-6">
+                  {bookmark.propertyInfo.name}
+                </dt>
+              </Link>
               <dd className="mt-1 sm:mt-0 sm:flex-auto">
-                <p>{bookmark.street}</p>
+                <p>{bookmark.propertyInfo.street}</p>
               </dd>
             </div>
           ))
